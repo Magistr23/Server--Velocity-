01399b15e12cb9991ca03640dd3818f4066e10c4
Иван Сорокин <spanhbob98@mail.ru>
2023-05-18 23:35:26 +0300

    init

---
+++ HUB/start.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cd /d "%~dp0"
+java -Xms2024M -Xmx4048M -jar purpur.jar nogui
---
+++ velocity/start.bat
@@ -0,0 +1,1 @@
+java -Xms2048M -Xmx2048M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
